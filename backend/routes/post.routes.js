20e6ae9d563cb522961707cda075dff8dfb61a9c
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-09-15 23:09:38 +0200

    added get all posts enedpoint

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentOnPost, createPost, deletePost, LikeUnlikePost } from "../controllers/post.controllers.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  LikeUnlikePost,
+} from "../controllers/post.controllers.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
 router.post("/create", protectRoute, createPost);
-router.post("/like/:id", protectRoute, LikeUnlikePost)
-router.post("/comment/:id", protectRoute, commentOnPost)
- router.delete("/:id", protectRoute, deletePost)
+router.post("/like/:id", protectRoute, LikeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
