22b5c00977ddee84269db84d055609745e2bd53e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-09-20 20:26:40 +0200

    added the controller for getting user posts

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -189,3 +189,43 @@ export const getLikedPost = async (req, res) => {
     res.status(500).json({ error: "internal server error" });
   }
 };
+
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({ path: "user", select: "-password" })
+      .populate({ path: "comments.user", select: "-password" });
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("error in getfollowing controller ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({ path: "user", select: "-password" })
+      .populate({ path: "comments.user", select: "-password" });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getUserPost controller: ", error);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -7,7 +7,9 @@ import {
   createPost,
   deletePost,
   getAllPosts,
+  getFollowingPosts,
   getLikedPost,
+  getUserPosts,
   LikeUnlikePost,
 } from "../controllers/post.controllers.js";
 
@@ -17,7 +19,9 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.get("/all", protectRoute, getAllPosts);
-router.get("/likes/:id", protectRoute, getLikedPost);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/likes/: ", protectRoute, getLikedPost);
 router.post("/create", protectRoute, upload.single("img"), createPost);
 router.post("/like/:id", protectRoute, LikeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
