33a25aed4c77654a4a402022ed00ab5cee4942d8
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-09-21 12:00:49 +0200

    UI designs completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/home/HomePage";
-import  SignUpPage  from "./pages/auth/signup/SignUpPage";
-import { LoginPage } from "./pages/auth/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/Notifications/NotificationsPage";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
